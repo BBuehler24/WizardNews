a47e8af73d3401ac8171a8a27b4a3273f5ad0899
BBuehler24 <bbuehler47@yahoo.com>
2023-05-06 12:34:50 -0500

    changes

--- app.js
+++ app.js
@@ -1,9 +1,30 @@
 const express = require("express");
 const app = express();
+const morgan = require('morgan');
+const postBank = require('./postBank');
+const allPosts = require('./allPosts');
+const individualPosts = require('./individualPosts');
 
-app.get("/", (req, res) => res.send("Hello World!"));
+app.use(morgan('dev'));
+
+app.get('/', (req, res) => {
+  const posts = postBank.list();
+  res.send(allPosts(posts));
+});
+
+app.get('/posts/:id', (req, res, next) => {
+  const id = req.params.id;
+  const post = postBank.find(id);
+  if (post.id) {
+    res.send(individualPosts(post));
+  } else {
+    next('404 Page Not Found')
+  }
+});
 
 const PORT = 1337;
+
+app.use(express.static('public'));
 
 app.listen(PORT, () => {
   console.log(`App listening in port ${PORT}`);
